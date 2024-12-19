05800dbfe2dafafd294f76a8b05cef3045f3689e
SophieHervieu <sophie-hervieu@live.fr>
2024-12-19 16:59:09 +0100

    update navbar

--- src/App.jsx
+++ src/App.jsx
@@ -9,7 +9,7 @@ function App() {
 
   return (
     <div className='app'>
-      <NavBar />
+      <NavBar className='nav'/>
     </div>
   )
 }
--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -1,12 +1,23 @@
 import { RiHomeFill } from "react-icons/ri";
+import { PiGithubLogo, PiLinkedinLogo } from "react-icons/pi";
+import { GoArrowUpRight } from "react-icons/go";
 
 export function NavBar() {
     return (
         <div className="nav">
-            <RiHomeFill />
-            <span>Projets</span>
-            <span>A propos</span>
-            <span>Contactez-moi !</span>
+            <div className="leftSection">
+                <RiHomeFill className="homeIcon"/>
+                <span>Projets</span>
+                <span>A propos</span>
+            </div>
+            <div className="rightSection">
+                <PiGithubLogo className="githubIcon"/>
+                <PiLinkedinLogo className="linkedinIcon"/>
+                <div className="contactBtn">
+                    <span>Contactez-moi !</span>
+                    <GoArrowUpRight className="arrowIcon"/>
+                </div>
+            </div>
         </div>
     );
 }
