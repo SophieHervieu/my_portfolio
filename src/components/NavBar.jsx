35af23c754a3b3e17554ccf3b2faa25054a58d38
SophieHervieu <sophie-hervieu@live.fr>
2024-12-20 16:30:27 +0100

    ajouts navbar

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -1,10 +1,32 @@
+import { useEffect, useState, useRef } from "react";
 import { RiHomeFill } from "react-icons/ri";
 import { PiGithubLogo, PiLinkedinLogo } from "react-icons/pi";
 import { GoArrowUpRight } from "react-icons/go";
 
-export function NavBar() {
+export const NavBar = () => {
+    const [showNav, setShowNav] = useState(true);
+  const lastScrollY = useRef(0); // Utilisation de useRef pour conserver la valeur mutable
+
+  useEffect(() => {
+    const handleScroll = () => {
+      if (window.scrollY > lastScrollY.current) {
+        // Scroll vers le bas : cache la navbar
+        setShowNav(false);
+      } else {
+        // Scroll vers le haut : affiche la navbar
+        setShowNav(true);
+      }
+      lastScrollY.current = window.scrollY; // Met à jour la valeur de lastScrollY
+    };
+
+    window.addEventListener("scroll", handleScroll);
+    return () => {
+      window.removeEventListener("scroll", handleScroll);
+    };
+  }, []);
+
     return (
-        <div className="nav">
+        <div className={`nav ${showNav ? "visible" : "hidden"}`}>
             <div className="leftSection">
                 <RiHomeFill className="homeIcon"/>
                 <span>Projets</span>
